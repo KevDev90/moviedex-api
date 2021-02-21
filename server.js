3047b17476d3fe66ec8a41bf7e751786ee7364f8
Kevin Johnson <kevinj902302@gmail.com>
2021-02-21 16:12:43 -0700

    Solves assignment

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.use(function validateBearerToken(req, res, next) {
   const apiToken = process.env.API_TOKEN
   console.log(apiToken, 'apiToken')
   const authToken = req.get('Authorization')
+  console.log(authToken, 'authToken')
   if (!authToken || authToken.split(' ')[1] !== apiToken) {
     return res.status(401).json({ error: 'Unauthorized request' })
   }
